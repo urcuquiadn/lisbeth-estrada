b593eb19a73920041f68cda1930a434c07887260
urcuquiadn <alcaldiaurcuquiadn.ok@hotmail.com>
2026-09-21 10:38:05 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -273,7 +273,8 @@ FORMULARIO DE CONTACTO
 ============================================================ */
 const contactoForm = document.getElementById('contactoForm');
 const formMessage = document.getElementById('formMessage');
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby_S_cCd_7PCDtr9HwU1kNbIF-Tdvce3IeW53IKjB-HDzUJLgBHrW6b0r7SXMPDOXF6/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby_S_cCd_7PCDtr9HwU1kNbIF-Tdvce3IeW53IKjB-HDzUJLgBHrW6b0r7SXMPDOXF6/exec
+    ';
 
 if (contactoForm) {
     contactoForm.addEventListener('submit', async (e) => {
